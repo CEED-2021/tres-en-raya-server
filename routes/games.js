6b13545a705f1df0266ba2d5a3b643320c2c4ed2
Alvaro Maceda <alvaro@alvaromaceda.es>
2022-02-05 12:36:07 +0100

    adds encoding and decoding functions

--- routes/games.js
+++ routes/games.js
@@ -1,17 +1,31 @@
 import express from 'express'
 import games from '../repositories/games_repository.js'
 import { asyncRoute, error } from '../lib/utils.js';
+import { enc, dec } from '../lib/crypt.js'
+
+const ENC_KEY = 'patata';
 
 const router = express.Router();
 
 router.get('/game/:id', asyncRoute(async (req, res) => {
+  // TO-DO: check if the game belongs to the user
   const id = parseInt(req.params.id);
   const game = await games.game(id)
 
   if(!game) return res.status(404).send(error('Unknown game'))
 
+  //TO-DO: send first movement
+
   res.send(game)
 }))
 
+
+router.get('/game/:id/movements/:movid', asyncRoute(async (req, res) => {
+
+  //TO-DO: decode movid to gameid/movement number
+  //https://www.section.io/engineering-education/data-encryption-and-decryption-in-node-js-using-crypto/
+
+  res.send('TO-DO')
+}))
 
 export default router;
